46f7e1cfe9e417c868d1ac5e9db1c70646c58c33
Erin Xu <148895066+ErinXU2004@users.noreply.github.com>
2025-07-06 17:36:15 +0800

    Update Repeat_time_up.v

--- src/Repeat_time_up.v
+++ src/Repeat_time_up.v
@@ -1,1 +1,23 @@
+module time_up 
+  (
+    input  clk,
+    input  rst_n,
+    input  trig,
+    output [31:0] cfg_max,
+    output reg [31:0] cnt
+  );
 
+  always@(posedge clk or negedge rst_n)
+  begin
+    if(!rst_n)
+        cnt <= 32'd0;
+    else if(~en)
+        cnt <= 32'd0; //recount when not enable
+    else  
+    begin                
+      if(cnt >= cfg_max)    //count back to zero when reach max
+          cnt <= 32'd0;
+      else
+          cnt <= cnt + 32'd1;  //counting
+    end 
+  end
